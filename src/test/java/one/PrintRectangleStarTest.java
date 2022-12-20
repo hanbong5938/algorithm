0be3f499d91c81b061883c922d320b511b3c229a
hbbae <hbbae@wecruitcorp.com>
2022-12-20 14:21:28 +0900

    add

---
+++ src/test/java/one/PrintRectangleStarTest.java
@@ -0,0 +1,23 @@
+package one;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class PrintRectangleStarTest {
+
+    @Test
+    void test() {
+        Assertions.assertEquals("*****\n*****\n*****", solution(5, 3));
+    }
+
+    public String solution(int a, int b) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < b; i++) {
+            if (i != 0) {
+                stringBuilder.append("\n");
+            }
+            stringBuilder.append("*".repeat(Math.max(0, a)));
+        }
+        return stringBuilder.toString();
+    }
+}
