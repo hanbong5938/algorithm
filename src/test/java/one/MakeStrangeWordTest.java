862c139ae5236581effa5166d411a8bdea2f3d62
han <hanbong5938@gmail.com>
2022-12-20 14:21:20 +0900

    add

--- src/test/java/one/MakeStrangeWordTest.java
+++ src/test/java/one/MakeStrangeWordTest.java
@@ -1,5 +1,6 @@
 package one;
 
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -14,22 +15,22 @@ class MakeStrangeWordTest {
         Assertions.assertEquals("TrY HeLlO  WoRlD    ", solution("tRy hello  WORLD    "));
     }
 
-    public String solution(String s) {
+    public String solution(@NotNull String s) {
         StringBuilder builder = new StringBuilder();
         char[] arr = s.toCharArray();
         int index = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == ' ') {
+        for (char c : arr) {
+            if (c == ' ') {
                 index = 0;
                 builder.append(' ');
                 continue;
             }
 
             if (index % 2 == 0) {
-                builder.append(String.valueOf(arr[i]).toUpperCase());
+                builder.append(String.valueOf(c).toUpperCase());
                 index++;
             } else {
-                builder.append(String.valueOf(arr[i]).toLowerCase());
+                builder.append(String.valueOf(c).toLowerCase());
                 index++;
             }
         }
