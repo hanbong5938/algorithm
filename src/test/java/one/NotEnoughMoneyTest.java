755bd60452253e47244e13067410f8f1e269d753
han <hanbong5938@gmail.com>
2022-12-22 11:30:14 +0900

    음양 더하기

---
+++ src/test/java/one/NotEnoughMoneyTest.java
@@ -0,0 +1,21 @@
+package one;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class NotEnoughMoneyTest {
+
+    @Test
+    void test() {
+        Assertions.assertEquals(10, solution(3, 20, 4));
+    }
+
+    public long solution(int price, int money, int count) {
+        long answer = 0;
+        for (int i = 1; i <= count; i++) {
+            answer += (long) price * (i);
+        }
+
+        return answer > money ? answer - money : 0;
+    }
+}
