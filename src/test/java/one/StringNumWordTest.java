3bb2e9a7f677ea34a59f2bbe8a302bbb1daf006c
han <hanbong5938@gmail.com>
2022-12-27 16:05:03 +0900

    문자 숫자 단어

---
+++ src/test/java/one/StringNumWordTest.java
@@ -0,0 +1,45 @@
+package one;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class StringNumWordTest {
+
+    @Test
+    void test() {
+        Assertions.assertEquals(1478, solution("one4seveneight"));
+        Assertions.assertEquals(234567, solution("23four5six7"));
+        Assertions.assertEquals(10203, solution("1zerotwozero3"));
+    }
+
+//숫자	영단어
+//0	zero
+//1	one
+//2	two
+//3	three
+//4	four
+//5	five
+//6	six
+//7	seven
+//8	eight
+//9	nine
+
+    public int solution(String s) {
+        return Integer.parseInt(
+                s.replace("zero", "0")
+                        .replace("one", "1")
+                        .replace("two", "2")
+                        .replace("three", "3")
+                        .replace("four", "4")
+                        .replace("five", "5")
+                        .replace("six", "6")
+                        .replace("seven", "7")
+                        .replace("eight", "8")
+                        .replace("nine", "9"));
+    }
+}
