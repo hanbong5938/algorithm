eac5f5e20f63b2b9a8145ca399c619eee0906ead
han <hanbong5938@gmail.com>
2022-12-28 08:25:46 +0900

    문자 숫자 단어

--- src/test/java/one/StringNumWordTest.java
+++ src/test/java/one/StringNumWordTest.java
@@ -17,18 +17,6 @@ class StringNumWordTest {
         Assertions.assertEquals(10203, solution("1zerotwozero3"));
     }
 
-//숫자	영단어
-//0	zero
-//1	one
-//2	two
-//3	three
-//4	four
-//5	five
-//6	six
-//7	seven
-//8	eight
-//9	nine
-
     public int solution(String s) {
         return Integer.parseInt(
                 s.replace("zero", "0")
