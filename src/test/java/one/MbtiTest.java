3ea113e1dc8202c19752be8546f7b47c5035e3c4
han <hanbong5938@gmail.com>
2022-12-28 09:29:28 +0900

    성격 유형 검사하기
    - 하나의 유형으로 통일
    - 1 ~ 7 의 점수를 -4 를 통해 -3 ~ 3 로 변환 한여 진행

---
+++ src/test/java/one/MbtiTest.java
@@ -0,0 +1,65 @@
+package one;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class MbtiTest {
+
+    @Test
+    void test() {
+        Assertions.assertEquals("TCMA", solution(new String[]{"AN", "CF", "MJ", "RT", "NA"}, new int[]{5, 3, 2, 7, 5}));
+        Assertions.assertEquals("RCJA", solution(new String[]{"TR", "RT", "TR"}, new int[]{7, 1, 3}));
+    }
+
+    public String solution(String[] survey, int[] choices) {
+        StringBuilder answer = new StringBuilder();
+        int[] score = new int[4];
+        for (int i = 0, surveyLength = survey.length; i < surveyLength; i++) {
+            String current = reverse(survey[i]);
+            int choice = current.equals(survey[i]) ? choices[i] - 4 : 4 - choices[i];
+            score[getSurveyIndex(current)] += choice;
+        }
+
+        for (int i = 0; i < score.length; i++) {
+            int num = score[i];
+            if (num >= 0) {
+                answer.append(getSurvey(i).charAt(1));
+            } else {
+                answer.append(getSurvey(i).charAt(0));
+            }
+        }
+
+        return answer.toString();
+    }
+
+    private String reverse(String s) {
+        return switch (s) {
+            case "RT" -> "TR";
+            case "CF" -> "FC";
+            case "JM" -> "MJ";
+            case "AN" -> "NA";
+            default -> s;
+        };
+    }
+
+    private int getSurveyIndex(String current) {
+        return switch (current) {
+            case "TR" -> 0;
+            case "FC" -> 1;
+            case "MJ" -> 2;
+            case "NA" -> 3;
+            default -> -1;
+        };
+    }
+
+    private String getSurvey(int current) {
+        return switch (current) {
+            case 0 -> "TR";
+            case 1 -> "FC";
+            case 2 -> "MJ";
+            case 3 -> "NA";
+            default -> "";
+        };
+    }
+
+}
