a73f1cc0dcbb5a327b977675bcb4ab653af50141
han <hanbong5938@gmail.com>
2022-12-29 09:05:28 +0900

    게임 맵 최단 거리
    - BFS 사용
    - visited 로 현재 좌표 까지 이동한 갯수 측정
    - queue 에 다음에 갈 좌표 입력
    - 마지막 좌표 값이 x == n - 1, y == m - 1 인 것을 이용 하여 answer 값 추출 ( 배열 인덱스 이기에 -1 )
    - 현제 좌표 에서 상, 하, 좌, 우 좌표 넘지 않는지, 지나 갈 수 있는지, 이미 다른 경로로 방문 했는지 확인 하여 계산 하고 queue 에 이동한 좌표 입력
    - 도달하지 못한 경우 -1

---
+++ src/test/java/two/MinimumDistanceTest.java
@@ -0,0 +1,61 @@
+
+package two;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+class MinimumDistanceTest {
+
+    @Test
+    void test() {
+        Assertions.assertEquals(11, solution(new int[][]{{1, 0, 1, 1, 1}, {1, 0, 1, 0, 1},
+                {1, 0, 1, 1, 1}, {1, 1, 1, 0, 1}, {0, 0, 0, 0, 1}}));
+        Assertions.assertEquals(-1, solution(new int[][]{{1, 0, 1, 1, 1}, {1, 0, 1, 0, 1},
+                {1, 0, 1, 1, 1}, {1, 1, 1, 0, 0}, {0, 0, 0, 0, 1}}));
+    }
+    public int solution(int[][] maps) {
+        int answer = 0;
+        Queue<int[]> queue = new LinkedList<>();
+        int n = maps.length;
+        int m = maps[0].length;
+        int[][] visited = new int[n][m];
+        queue.add(new int[]{0, 0});
+        while (!queue.isEmpty()) {
+            int[] current = queue.poll();
+            int x = current[0];
+            int y = current[1];
+
+            if (x == n - 1 && y == m - 1) {
+                answer = visited[x][y] + 1;
+                break;
+            }
+
+            int left = x - 1;
+            int right = x + 1;
+            int top = y + 1;
+            int bottom = y - 1;
+            if (left >= 0 && maps[left][y] == 1 && visited[left][y] == 0) {
+                visited[left][y] = visited[x][y] + 1;
+                queue.add(new int[]{left, y});
+            }
+            if (right < n && maps[right][y] == 1 && visited[right][y] == 0) {
+                visited[right][y] = visited[x][y] + 1;
+                queue.add(new int[]{right, y});
+            }
+            if (top < m && maps[x][top] == 1 && visited[x][top] == 0) {
+                visited[x][top] = visited[x][y] + 1;
+                queue.add(new int[]{x, top});
+            }
+            if (bottom >= 0 && maps[x][bottom] == 1 && visited[x][bottom] == 0) {
+                visited[x][bottom] = visited[x][y] + 1;
+                queue.add(new int[]{x, bottom});
+            }
+
+
+        }
+        return answer == 0 ? -1 : answer;
+    }
+}
