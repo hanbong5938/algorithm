a1564f2234ed39be294b3653ab49476db2925668
han <hanbong5938@gmail.com>
2023-01-03 08:46:39 +0900

    올바른 괄호
    - stack 을 활용
    - solution2 는 다른 사람이 stack 사용 하지 않은 풀이가 있어서 가져와 봄

---
+++ src/test/java/two/CollectBlockTest.java
@@ -0,0 +1,55 @@
+
+package two;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Stack;
+
+class CollectBlockTest {
+
+    @Test
+    void test() {
+        Assertions.assertTrue(solution("()()"));
+        Assertions.assertTrue(solution("(())()"));
+        Assertions.assertFalse(solution(")()("));
+        Assertions.assertFalse(solution("(()("));
+        Assertions.assertTrue(solution2("()()"));
+        Assertions.assertTrue(solution2("(())()"));
+        Assertions.assertFalse(solution2(")()("));
+        Assertions.assertFalse(solution2("(()("));
+    }
+
+    boolean solution(String s) {
+        Stack<Character> stack = new Stack<>();
+        char[] arr = s.toCharArray();
+        for (char c : arr) {
+            if (c == '(') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    boolean solution2(String s) {
+        int count = 0;
+        char[] arr = s.toCharArray();
+        for (char c : arr) {
+            if (c == '(') {
+                count++;
+            } else {
+                count--;
+            }
+            if (count < 0) {
+                return false;
+            }
+        }
+        return count == 0;
+    }
+}
