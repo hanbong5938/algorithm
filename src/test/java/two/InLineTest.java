bb88b65f7133d894ee651dfc7d17d7231fc9dbf4
han <hanbong5938@gmail.com>
2023-01-06 09:11:43 +0900

    줄 서는 방법
    - factorial 을 통하여 경우의 수를 구한다.
    - human list 의 index 는 0 부터 시작하며 이를 위해 k--를 해준다
    - 경우의 수를 n 으로 나누면 앞의 숫자가 바뀐다는 것을 이용한다. ex) [1, 2, 3] -> [2, 1, 3]
    - 첫번째 while 문이 돌때 앞의 숫자가 바뀌는 수를 구한 다음 이를 k 로 나눠 주면 현재 앞의 숫자 값을 human 에서 가져올 수 잇다.
    - 줄을 선 사람은 중복되지 않아 삭제 한다
    - k % numberOfCase 로 처리된 나머지 숫자를 구한다
    - list 가 줄었기에 n-- 처리

---
+++ src/test/java/two/InLineTest.java
@@ -0,0 +1,40 @@
+
+package two;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class InLineTest {
+
+    @Test
+    void test() {
+        Assertions.assertArrayEquals(new int[]{3, 1, 2}, solution(3, 5));
+    }
+
+    public int[] solution(int n, long k) {
+        int[] answer = new int[n];
+
+        List<Integer> human = new ArrayList<>();
+        long numberOfCase = 1;
+        for (int i = 1; i <= n; i++) {
+            numberOfCase *= i;
+            human.add(i);
+        }
+
+        k--;
+        int idx = 0;
+        while (n > 0) {
+            numberOfCase /= n;
+            int current = (int) (k / numberOfCase);
+            answer[idx] = human.get(current);
+            human.remove(current);
+            k %= numberOfCase;
+            idx++;
+            n--;
+        }
+        return answer;
+    }
+}
