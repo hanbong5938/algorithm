b24ce6ad3f2f92e3843a8d583e791f8a1cd0b7f6
han <hanbong5938@gmail.com>
2023-01-11 09:10:03 +0900

    라이브러리 버전 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,10 +10,10 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains:annotations:23.0.0")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
+    implementation("org.jetbrains:annotations:23.1.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.1")
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.1")
 }
 
 tasks.getByName<Test>("test") {
