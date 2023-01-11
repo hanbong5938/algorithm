7e3ddefde06ab4a32b1a9fe037c5983a50f4e63a
han <hanbong5938@gmail.com>
2023-01-12 08:59:40 +0900

    여러 숫자 최소 공배수
    - 각각 숫자를 반복문을 통해 최대 공약수를 구한 다음 m * n / gcd 를 통하여 계산

---
+++ src/test/java/two/LCMTest.java
@@ -0,0 +1,33 @@
+
+package two;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+class LCMTest {
+
+    @Test
+    void test() {
+        Assertions.assertEquals(168, solution(new int[]{2, 6, 8, 14}));
+        Assertions.assertEquals(6, solution(new int[]{1, 2, 3}));
+        Assertions.assertEquals(144, solution(new int[]{3, 4, 9, 16}));
+    }
+
+    public int solution(int[] arr) {
+        int answer = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            int gcd = gcd(answer, arr[i]);
+            answer = answer * arr[i] / gcd;
+        }
+        return answer;
+    }
+
+    private int gcd(int gcd, int i) {
+        if (i == 0) {
+            return gcd;
+        }
+        return gcd(i, gcd % i);
+    }
+
+}
