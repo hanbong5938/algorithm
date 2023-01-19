e9da73d61f1328c3df907bc2473d7ecb0c432abc
han <hanbong5938@gmail.com>
2023-01-19 10:00:24 +0900

    큰 수 만들기
    - 탐욕법
    - stack 이나 string 변환 하는 형태로는 10번 성능 테스트 실패
    - index 활용하여 접근 하는 형태로 변경

---
+++ src/test/java/programmers/two/MakeBigNumberTest.java
@@ -0,0 +1,39 @@
+
+package programmers.two;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+class MakeBigNumberTest {
+
+    @Test
+    void test() {
+        Assertions.assertEquals("94", solution("1924", 2));
+        Assertions.assertEquals("3234", solution("1231234", 3));
+        Assertions.assertEquals("775841", solution("4177252841", 4));
+        Assertions.assertEquals("234", solution("01234", 2));
+        Assertions.assertEquals("432", solution("4321", 1));
+
+    }
+
+    public String solution(String number, int k) {
+        StringBuilder answer = new StringBuilder();
+        int idx = 0;
+        for (int i = 0; i < number.length() - k; i++) {
+
+            char max = 0;
+            for(int j = idx; j <= i + k; j++) {
+                char current = number.charAt(j);
+                if(max < current) {
+                    max = current;
+                    idx = j + 1;
+                }
+            }
+            answer.append(max);
+        }
+
+        return answer.toString();
+    }
+}
