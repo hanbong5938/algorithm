cc45fe427f066d2eb3a4e5752eba31eed72d6aae
han <hanbong5938@gmail.com>
2023-02-09 08:25:24 +0900

    이진 변환 반복하기

---
+++ src/test/java/programmers/two/RepeatBinaryTest.java
@@ -0,0 +1,36 @@
+
+package programmers.two;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+class RepeatBinaryTest {
+
+    @Test
+    void test() {
+        Assertions.assertArrayEquals(new int[]{3, 8}, solution("110010101001"));
+        Assertions.assertArrayEquals(new int[]{3, 3}, solution("01110"));
+        Assertions.assertArrayEquals(new int[]{4, 1}, solution("1111111"));
+    }
+
+    public int[] solution(String s) {
+        int count = 0;
+        int zeroCount = 0;
+
+        while (!s.equals("1")) {
+            int originalLength = s.length();
+            int removeLength = s.replace("0", "").length();
+            zeroCount += originalLength - removeLength;
+            s = Integer.toBinaryString(removeLength);
+            count++;
+        }
+
+        return new int[]{count, zeroCount};
+    }
+
+}
