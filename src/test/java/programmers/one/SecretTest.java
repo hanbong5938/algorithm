3ec7f0d1be41cb6ff103593db5835c471e36801e
han <hanbong5938@gmail.com>
2023-02-15 08:23:35 +0900

    둘만의 암호

---
+++ src/test/java/programmers/one/SecretTest.java
@@ -0,0 +1,32 @@
+
+package programmers.one;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+
+class SecretTest {
+
+    @Test
+    void test() {
+        Assertions.assertEquals("happy", solution("aukks", "wbqd", 5));
+    }
+
+    public String solution(String s, String skip, int index) {
+        StringBuilder answer = new StringBuilder();
+        List<String> list = new LinkedList<>();
+        for (int i = 0; i < 26; i++) {
+            String str = String.valueOf((char) (i + 'a'));
+            if (!skip.contains(str)) {
+                list.add(str);
+            }
+        }
+
+        for (char c : s.toCharArray()) {
+            answer.append(list.get((list.indexOf(String.valueOf(c)) + index) % list.size()));
+        }
+        return answer.toString();
+    }
+}
