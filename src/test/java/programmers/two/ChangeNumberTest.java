ca4dc3c0529c1fc11d3ba278ae0d9cc8d3cdc11d
han <hanbong5938@gmail.com>
2023-03-23 09:34:02 +0900

    숫자 변환 하기

---
+++ src/test/java/programmers/two/ChangeNumberTest.java
@@ -0,0 +1,43 @@
+package programmers.two;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class ChangeNumberTest {
+
+    @Test
+    void test() {
+        Assertions.assertEquals(2, solution(10, 40, 5));
+        Assertions.assertEquals(1, solution(10, 40, 30));
+        Assertions.assertEquals(-1, solution(2, 5, 4));
+    }
+
+    public int solution(int x, int y, int n) {
+        int[] dp = new int[y + 1];
+
+        for (int i = x + 1; i < y + 1; i++) {
+            int a = getDivide(x, dp, i, 2);
+            int b = getDivide(x, dp, i, 3);
+            int c = aboveUnderX(x, i - n) ? dp[i - n] : Integer.MAX_VALUE;
+
+            // 숫자 i를 만들기 위한 최소 방법을 찾음
+            int d = Math.min(Math.min(a, b), c);
+
+            // 만들 수 있으면 d+1 저장
+            // 만들 수 없다면 MAX 저장
+            dp[i] = d == Integer.MAX_VALUE ? Integer.MAX_VALUE : d + 1;
+        }
+        return dp[y] == Integer.MAX_VALUE ? -1 : dp[y];
+    }
+
+    private int getDivide(int x, int[] dp, int i, int divide) {
+        return (i / divide > 0 && i % divide == 0) && aboveUnderX(x, i / divide) ?
+                dp[i / divide] : Integer.MAX_VALUE;
+    }
+
+    private boolean aboveUnderX(int x, int num) {
+        return x <= num;
+    }
+
+
+}
