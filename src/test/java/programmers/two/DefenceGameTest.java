bf74a020925522160ad88b03f4f68d545e1619b5
han <hanbong5938@gmail.com>
2023-05-15 08:52:42 +0900

    디펜스 게임 테스트

---
+++ src/test/java/programmers/two/DefenceGameTest.java
@@ -0,0 +1,32 @@
+
+package programmers.two;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+class DefenceGameTest {
+
+    @Test
+    void test() {
+        Assertions.assertEquals(5, solution(7, 3, new int[]{4, 2, 4, 5, 3, 3, 1}));
+        Assertions.assertEquals(4, solution(2, 4, new int[]{3, 3, 3, 3}));
+    }
+
+    public int solution(int n, int k, int[] enemy) {
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+        for (int i = 0; i < enemy.length; i++) {
+            int current = enemy[i];
+            priorityQueue.add(current);
+            if (priorityQueue.size() > k) {
+                n -= priorityQueue.poll();
+            }
+            if (n < 0) {
+                return i;
+            }
+        }
+        return enemy.length;
+    }
+
+}
