07fe0a76d55f301b50c8620647223c022f3007b3
han <hanbong5938@gmail.com>
2023-08-22 09:30:50 +0900

    조이 스틱

---
+++ src/test/java/programmers/two/JoyStickTest.java
@@ -0,0 +1,46 @@
+package programmers.two;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Stack;
+
+class JoyStickTest {
+
+    @Test
+    void test() {
+        Assertions.assertEquals(11, solution("JAZ"));
+        Assertions.assertEquals(56, solution("JEROEN"));
+        Assertions.assertEquals(23, solution("JAN"));
+        Assertions.assertEquals(6, solution("BBAAAB"));
+        Assertions.assertEquals(16, solution("AAAAABBAAAAAAABAAA"));
+    }
+
+    public int solution(String name) {
+        int answer = 0;
+        int length = name.length();
+
+        for (int i = 0; i < name.length(); i++) {
+            answer += Math.min(name.charAt(i) - 'A', 'Z' - name.charAt(i) + 1);
+        }
+
+        int move = length - 1;
+
+        for (int i = 0; i < name.length(); i++) {
+            int index = i + 1;
+            while (index < length && name.charAt(index) == 'A') {
+                index++;
+            }
+
+            move = Math.min(Math.min(move, i * 2 + length - index), (length - index) * 2 + i);
+        }
+
+        return answer + move;
+    }
+}
